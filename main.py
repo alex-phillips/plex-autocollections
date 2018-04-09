ccccac4938c2ebfdc6be4533d49a2c3faf43e090
alex-phillips <ahp118@gmail.com>
2018-04-09 18:41:06 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# Austin Hasten
+# Initial Commit - November 16th, 2017
+# TODO Allow selection of multiple sections
+# TODO Allow multiple keywords
+# TODO Cache series
+
+import re, sys, time, getpass, logging
+import plexapi.utils
+from retry import retry
+from plexapi.myplex import MyPlexAccount
+from plexapi.exceptions import BadRequest
+import yaml
+
+class Plex():
+    def __init__(self):
+        self.account = self.get_account()
+        self.server = self.get_account_server(self.account)
+        self.section = self.get_server_section(self.server)
+        self.media = self.get_flat_media(self.section)
+
+    @retry(BadRequest)
+    def get_account(self):
+        username = input("Plex Username: ")
+        password = getpass.getpass()
+
+        return MyPlexAccount(username, password)
+
+    def get_account_server(self, account):
+        servers = [ _ for _ in account.resources() if _.product == 'Plex Media Server' ]
+        if not servers:
+            print('No available servers.')
+            sys.exit()
+
+        return plexapi.utils.choose('Select server index', servers, 'name').connect()
+
+    def get_server_section(self, server):
+        sections = [ _ for _ in server.library.sections() if _.type in {'movie'} ]
+        if not sections:
+            print('No available sections.')
+            sys.exit()
+
+        return plexapi.utils.choose('Select section index', sections, 'title')
+
+    def get_flat_media(self, section):
+        # Movie sections are already flat
+        if section.type == 'movie':
+            return self.section.all()
+        else:
+            episodes = []
+            for show in self.section.all():
+                episodes += show.episodes()
+            return episodes
+
+with (open("collections.yml", "r")) as stream:
+    collections = yaml.load(stream)
+
+if __name__ == "__main__":
+    plex = Plex()
+    keyword_matches = []
+
+    for medium in plex.media:
+        for collection, movies in collections.items():
+            matches = []
+            for movie in movies:
+                regex = re.compile(movie, re.IGNORECASE)
+                if re.search(regex, medium.title):
+                    print("Adding", medium.title, "to collection", collection)
+                    matches.append(medium)
+            if matches:
+                for movie in matches:
+                    movie.addCollection(collection)
