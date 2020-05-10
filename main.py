c2af06d2d02c7cda3c07a4bb7548611349c79dc2
Tom Hoover <tom@hisword.net>
2020-05-10 10:02:59 -0500

    Add ability to select LIBRARY via the command line
    * pass the name of a LIBRARY via the '--library' command line option
    * add '--help' option
    * refactor code to add a main() function
    * rename color CONSTANTS as uppercase for compliance with PEP 8 style guide
    
    Closes #42

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from plexapi.server import PlexServer, CONFIG
 from plexapi.myplex import MyPlexAccount
 from plexapi.exceptions import BadRequest
 import yaml
-import glob, os
+import glob, os, argparse
 
 ## Edit ##
 PLEX_URL = ''
 PLEX_TOKEN = ''
 
 DEBUG = os.getenv('DEBUG')
-Default = '\033[0m'  # reset to default text color
-Red     = '\033[31m' # set text color to red
-Green   = '\033[32m' # set text color to green
-Blue    = '\033[34m' # set text color to blue
+RESET_COLOR = '\033[0m'  # reset to default text color
+RED         = '\033[31m' # set text color to red
+GREEN       = '\033[32m' # set text color to green
+BLUE        = '\033[34m' # set text color to blue
 
 try:
     PLEX_URL = CONFIG.data['auth'].get('server_baseurl', PLEX_URL)
@@ -25,14 +25,21 @@ except:
     print("Failed loading in config file.")
 
 class Plex():
-    def __init__(self):
+    def __init__(self, library=""):
         if PLEX_URL and PLEX_TOKEN:
             self.server = PlexServer(PLEX_URL, PLEX_TOKEN)
         else:
             self.account = self.get_account()
             self.server = self.get_account_server(self.account)
 
-        self.section = self.get_server_section(self.server)
+        if library:
+            try:
+                self.section = self.server.library.section(library)
+            except plexapi.exceptions.NotFound:
+                self.section = self.get_server_section(self.server)
+        else:
+            self.section = self.get_server_section(self.server)
+
         self.media = self.get_flat_media(self.section)
 
     @retry(BadRequest)
@@ -82,46 +89,59 @@ def process_movies(movies, medium, collection):
             regex = re.compile(movie, re.IGNORECASE)
             if re.search(regex, medium.title):
                 if year_regex and re.search(year_regex, str(medium.year)):
-                    print("Adding" + Red, medium.title, Default + "to collection" + Blue, collection, Default)
+                    print("Adding" + RED, medium.title, RESET_COLOR + "to collection" + BLUE, collection, RESET_COLOR)
                     matches.append(medium)
                 elif year_regex is None:
-                    print("Adding" + Red, medium.title, Default + "to collection" + Blue, collection, Default)
+                    print("Adding" + RED, medium.title, RESET_COLOR + "to collection" + BLUE, collection, RESET_COLOR)
                     matches.append(medium)
 
     if matches:
         for movie in matches:
             movie.addCollection(collection)
 
-def read_collection(filename):
+def read_collection(filename, collections):
     if ((os.path.isfile(filename) > 0) and (os.path.getsize(filename) > 0)):
         with (open(filename, "r")) as stream:
             collections.update(yaml.load(stream, Loader=yaml.SafeLoader))
-            print(Green + 'Reading ' + filename + '...' + Default)
+            print(GREEN + 'Reading ' + filename + '...' + RESET_COLOR)
             if DEBUG:
                 for k, v in collections.items():
-                    print(Blue, k, "->", v, Default)
-                print(Red, collections, Default)
+                    print(BLUE, k, "->", v, RESET_COLOR)
+                print(RED, collections, RESET_COLOR)
     else:
         print()
-        print(Red + filename + Blue, 'is missing or empty. Skipping...' + Default)
+        print(RED + filename + BLUE, 'is missing or empty. Skipping...' + RESET_COLOR)
         print()
 
-print()
-collections = {}        # create empty dictionary
-total = len(sys.argv)
-if total >= 2:          # at least one filename has been passed as an argument
-    for i in range(1,total):
-        read_collection(sys.argv[i])
-else:
-    read_collection('collections.yml')
-    custom_collections = glob.glob('collections.d/*.yml')
-    for custom_collection in custom_collections:
-        read_collection(custom_collection)
+def main():
+    parser = argparse.ArgumentParser(description="Automatically create Plex collections")
+    parser.add_argument("-l", "--library", help="choose LIBRARY from CLI")
+    parser.add_argument("collection", nargs="*")
+    args = parser.parse_args()
 
-if __name__ == "__main__":
-    plex = Plex()
-    keyword_matches = []
+    if args.library:
+        plex = Plex(args.library)
+    else:
+        plex = Plex()
+
+    print()
+    collections = {}        # create empty dictionary
+
+    if args.collection:
+        for i in range(len(args.collection)):
+            read_collection(args.collection[i], collections)
+    else:
+        read_collection('collections.yml', collections)
+        custom_collections = glob.glob('collections.d/*.yml')
+        for custom_collection in custom_collections:
+            read_collection(custom_collection, collections)
+
+    print()
+    # keyword_matches = []  # unused list?
 
     for medium in plex.media:
         for collection, movies in collections.items():
             process_movies(movies, medium, collection)
+
+if __name__ == "__main__":
+    main()
